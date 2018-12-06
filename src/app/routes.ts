0628634c018f323580e009332bf916e45bf5b04a
Mtbd83 <m.tribaudeau@gmail.com>
2018-12-06 09:27:32 +0100

    Updated route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,6 +3,10 @@ import {PlanningComponent} from './planning/planning.component';
 import {HomeComponent} from './home/home.component';
 import {FormateurComponent} from './formateur/formateur.component';
 import {EditFormateurComponent} from './edit-formateur/edit-formateur.component';
+import {ModuleComponent} from './module/module.component';
+import {EditModuleComponent} from './edit-module/edit-module.component';
+import {EditPlanningComponent} from './edit-planning/edit-planning.component';
+
 
 export const routes: Routes = [
   {path: 'planning', component: PlanningComponent},
@@ -10,5 +14,11 @@ export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'formateur', component: FormateurComponent},
   {path: 'formateur/edit', component: EditFormateurComponent},
-  {path: 'formateur/edit/:id', component: EditFormateurComponent}
+  {path: 'formateur/edit/:id', component: EditFormateurComponent},
+  {path: 'module', component: ModuleComponent},
+  {path: 'module/edit', component: EditModuleComponent},
+  {path: 'module/edit/:idModule', component: EditModuleComponent},
+  {path: 'planning', component: ModuleComponent},
+  {path: 'planning/edit', component: EditPlanningComponent},
+  {path: 'planning/edit/:idModule', component:  EditPlanningComponent}
 ];
